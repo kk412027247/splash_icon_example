312a72b54a2b8f7ed8ea66ddee5d06b323785163
TMD <1@123.com>
2018-07-10 00:46:39 +0800

    6

--- android/app/src/main/java/com/splash_icon_example/MainApplication.java
+++ android/app/src/main/java/com/splash_icon_example/MainApplication.java
@@ -3,6 +3,7 @@ package com.splash_icon_example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import org.reactnative.camera.RNCameraPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNCameraPackage(),
             new SplashScreenReactPackage()
       );
